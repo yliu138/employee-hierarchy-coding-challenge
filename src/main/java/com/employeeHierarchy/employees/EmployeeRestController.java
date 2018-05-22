b45cbf61b2661369de0129747760be1b293aaee7
Leo Liu <leo.liu@enmasse.com.au>
2018-05-23 00:47:12 +1000

    added a new service for getting the map of the employee

--- src/main/java/com/employeeHierarchy/employees/EmployeeRestController.java
+++ src/main/java/com/employeeHierarchy/employees/EmployeeRestController.java
@@ -4,6 +4,8 @@
 package com.employeeHierarchy.employees;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,5 +28,14 @@ public class EmployeeRestController {
 	@GetMapping
 	Collection<Employee> getAllEmployee() {
 		return this.employeeRepo.findAll();
+	}
+	
+	@GetMapping("/map")
+	Map<Long, Employee> getAllEmployeeMap() {
+		Map<Long, Employee> employeeMap = new HashMap<Long, Employee>();
+		for (Employee e: this.employeeRepo.findAll()) {
+			employeeMap.put(e.getId(), e);
+		}
+		return employeeMap;
 	}
 }
