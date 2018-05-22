9832dbbb758dd0ca69e6d90d345c9ffca4602000
Leo Liu <leo.liu@enmasse.com.au>
2018-05-22 15:16:38 +1000

    initialized the project

---
+++ src/main/java/com/employeeHierarchy/demo/EmployeeHierarchyApplication.java
@@ -0,0 +1,12 @@
+package com.employeeHierarchy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeHierarchyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeHierarchyApplication.class, args);
+	}
+}
