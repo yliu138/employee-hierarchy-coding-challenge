db524bf594a7c3498a70cabec509a592c169c218
Leo Liu <leo.liu@enmasse.com.au>
2018-05-25 11:54:36 +1000

    added the repository annotation tag to the repo interface so that the compiler will run faster

--- src/main/java/com/employeeHierarchy/repo/EmployeeRepo.java
+++ src/main/java/com/employeeHierarchy/repo/EmployeeRepo.java
@@ -6,6 +6,7 @@ package com.employeeHierarchy.repo;
 import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.employeeHierarchy.models.Employee;
 
@@ -13,6 +14,7 @@ import com.employeeHierarchy.models.Employee;
  * @author leoliu
  *
  */
+@Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 	Collection<Employee> findEmployeeByName(String name);
 }
