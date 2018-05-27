c79028c00e84e3de6ab47743b50f149b2c2c8972
Leo Liu <leo.liu@enmasse.com.au>
2018-05-27 11:24:30 +1000

    added cors support to the employee controller

--- src/main/java/com/employeeHierarchy/employees/EmployeeRestController.java
+++ src/main/java/com/employeeHierarchy/employees/EmployeeRestController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ import com.employeeHierarchy.repo.EmployeeRepo;
  */
 @RestController
 @RequestMapping("/employee")
+@CrossOrigin
 public class EmployeeRestController {
 	
 	@Autowired
